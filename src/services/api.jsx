efc9a98bcda6b33ecab9eb5b83f3ea88f5d53234
Elijah Apreko <elijahapreko@gmail.com>
2024-07-11 16:13:32 +0000

    add a debugger

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
+console.log(import.meta.env.VITE_APP_API_URL);
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
